25c4802da0cb6ca2160c245dfe38de4a2dacd8b9
Ben Stuart <benarthurstuart@hotmail.com>
2019-12-16 15:50:08 +0000

    refactored display functions for credit/debit, and slimmed statementCalc

--- src/Bank.js
+++ src/Bank.js
@@ -6,13 +6,12 @@ function Bank() {
 Bank.prototype.statementCalc = function() {
 
   var output = "";
-  for ( i=0; i<this.arrOutput.length ;i++) {
-    if (i === this.arrOutput.length-1) {
-      output += this.arrOutput[i];
-    } else {
-      output += this.arrOutput[i] + "\n";
-    }
+
+  for ( i=0; i<this.arrOutput.length; i++) {
+    output += this.arrOutput[i] + "\n";
   }
+
+  output = output.substring(0,output.length-1)
   return output;
 };
 
@@ -23,14 +22,22 @@ Bank.prototype.statementShow = function() {
 Bank.prototype.credit = function(sum) {
   this.balance += sum;
 
-  var d = new Date();
-  this.arrOutput.push(d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear().toString().charAt(2)+
-  d.getFullYear().toString().charAt(3)+' || '+Number(sum).toFixed(2)+' || || '+Number(this.balance).toFixed(2));
+  this.addCreditDisplay(sum);
 };
 
 Bank.prototype.debit = function(sum) {
   this.balance -= sum;
 
+  this.addDebitDisplay(sum);
+};
+
+Bank.prototype.addCreditDisplay = function(sum) {
+  var d = new Date();
+  this.arrOutput.push(d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear().toString().charAt(2)+
+  d.getFullYear().toString().charAt(3)+' || '+Number(sum).toFixed(2)+' || || '+Number(this.balance).toFixed(2));
+};
+
+Bank.prototype.addDebitDisplay = function(sum) {
   var d = new Date();
   this.arrOutput.push(d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear().toString().charAt(2)+
   d.getFullYear().toString().charAt(3)+' || || '+Number(sum).toFixed(2)+' || '+Number(this.balance).toFixed(2));
