5719fdc2c1aaa37ff901a780a5fa4c74440ae138
Ben Stuart <benarthurstuart@hotmail.com>
2019-12-16 14:47:54 +0000

    adds debit, and display for crediting

--- spec/BankSpec.js
+++ spec/BankSpec.js
@@ -1,8 +1,13 @@
 describe("Bank", function() {
   describe("After creating a new bank account", function() {
-    var bankAccount = new Bank();
 
-    it("statementShow should return an empty statement", function() {
+    var bankAccount;
+
+    beforeEach(function() {
+      bankAccount = new Bank();
+    });
+
+    it("statementShow returns an empty statement", function() {
       expect(bankAccount.statementCalc()).toEqual("date || credit || debit || balance");
     });
 
@@ -10,9 +15,28 @@ describe("Bank", function() {
       expect(bankAccount.balance).toEqual(0);
     });
 
-    it("increasing the balance by 250", function() {
+    it("crediting the balance by 250", function() {
       bankAccount.credit(250);
       expect(bankAccount.balance).toEqual(250);
+    });
+
+    it("credit the balance by 250 then debiting the balance by 10", function() {
+      bankAccount.credit(250);
+      bankAccount.debit(10);
+      expect(bankAccount.balance).toEqual(240);
+    });
+
+    it("display after credit of 200", function() {
+      bankAccount.credit(200);
+      expect(bankAccount.statementCalc()).toEqual("date || credit || debit || balance\n"
+       + "16/12/19 || 200 || || 200");
+    });
+
+    it("display after credit of 200 and 250", function() {
+      bankAccount.credit(200);
+      bankAccount.credit(250);
+      expect(bankAccount.statementCalc()).toEqual("date || credit || debit || balance\n"
+       + "16/12/19 || 200 || || 200\n16/12/19 || 250 || || 450");
     });
   });
 });
--- src/Bank.js
+++ src/Bank.js
@@ -1,9 +1,19 @@
 function Bank() {
   this.balance = 0;
+  this.arrOutput = ["date || credit || debit || balance"];
 };
 
 Bank.prototype.statementCalc = function() {
-  return "date || credit || debit || balance";
+
+  var output = "";
+  for ( i=0; i<this.arrOutput.length ;i++) {
+    if (i === this.arrOutput.length-1) {
+      output += this.arrOutput[i];
+    } else {
+      output += this.arrOutput[i] + "\n";
+    }
+  }
+  return output;
 };
 
 Bank.prototype.statementShow = function() {
@@ -12,4 +22,11 @@ Bank.prototype.statementShow = function() {
 
 Bank.prototype.credit = function(sum) {
   this.balance += sum;
+  var d = new Date();
+  this.arrOutput.push(d.getDate()+'/'+(d.getMonth()+1)+'/'+d.getFullYear().toString().charAt(2)+
+  d.getFullYear().toString().charAt(3)+' || '+sum+' || || '+this.balance);
+};
+
+Bank.prototype.debit = function(sum) {
+  this.balance -= sum;
 };
